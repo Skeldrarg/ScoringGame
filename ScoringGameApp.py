401c0e0c56c2ae2f3ab4ec921770697954bd0dc0
Skeldrarg <claudetab59@gmail.com>
2022-12-03 22:48:36 +0100

    Fichier kv pour gestion de la GUI

--- ScoringGameApp.py
+++ ScoringGameApp.py
@@ -1,35 +1,33 @@
-import kivy
 #kivy.require('2.1.0') # replace with your current kivy version !
 
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import ObjectProperty
 
-class Team():
+class TeamLayout(BoxLayout):
 
-    def __init__(self, name):
-        self.__name = name
-        self.__points = 0
-
-    def get_name(self):
-        return self.__name
-
+    points = 0
+    teamName = ""
     def get_points(self):
-        return self.__points
+        return self.points
 
     def set_points(self, points):
-        self.__points = points
+        self.points = points
 
-class InfoTeam(BoxLayout):
+    def add_points(self, point):
+        self.set_points(self.get_points()+point)
+        self.ids.points_label.text = str(self.get_points())
+
+    def set_team_name(self):
+        self.teamName = self.ids.teamnameinput.text
+        self.remove_widget(self.ids.teamname)
+        self.add_widget(Label(text=self.teamName, font_size=30), index=2)
+
+class IntroScreen(Screen):
     pass
-
-class CreateTeamScreen(Screen):
-    pass
-
-    def create_team(self, name):
-        team = Team(name)
-        print(team.get_name())
 
 class ScoringScreen(Screen):
     pass
@@ -40,11 +38,10 @@ class ScoringGameScreenManager(ScreenManager):
 class ScoringGameApp(App):
 
     def build(self):
-        return Builder.load_file("my.kv")
+        return Builder.load_file("main.kv")
 
 def main():
     ScoringGameApp().run()
-
 
 if __name__ == '__main__':
     main()
